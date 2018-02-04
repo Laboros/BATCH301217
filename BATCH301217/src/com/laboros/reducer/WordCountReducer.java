626d48ef9404b9c3c00806c6d485456aadfe85bb
svaduka <sainagaraju.vaduka@gmail.com>
2018-02-04 10:05:10 +0530

    WordCount MapReduce program

---
+++ BATCH301217/src/com/laboros/reducer/WordCountReducer.java
@@ -0,0 +1,34 @@
+package com.laboros.reducer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class WordCountReducer extends
+		Reducer<Text, IntWritable, Text, IntWritable> {
+
+	protected void setup(
+			org.apache.hadoop.mapreduce.Reducer<Text, IntWritable, Text, IntWritable>.Context arg0)
+			throws java.io.IOException, InterruptedException {
+	};
+
+	protected void reduce(
+			Text key,
+			java.lang.Iterable<IntWritable> values,
+			Context context)
+			throws java.io.IOException, InterruptedException 
+			{
+		//key -- CAT
+		//values -- {1,1,1,1}
+		int sum=0;
+		for (IntWritable intWritable : values) {
+			sum = sum+intWritable.get();
+		}
+		context.write(key, new IntWritable(sum));
+	};
+
+	protected void cleanup(
+			org.apache.hadoop.mapreduce.Reducer<Text, IntWritable, Text, IntWritable>.Context arg0)
+			throws java.io.IOException, InterruptedException {
+	};
+}
